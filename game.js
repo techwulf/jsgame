0895949cc426edde15c1217146cd0b96a57cc64e
techwulf <t.zach.moore@gmail.com>
2017-08-17 11:05:40 -0600

    add character piece

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
+var myGameChar;
+
 function startGame() {
 	myGameArea.start();
+	myGameChar = new component(30,30,"red",10,120);
 }
 
 var myGameArea = {
@@ -12,4 +15,14 @@ var myGameArea = {
 		document.body.insertBefore(this.canvas,
 			document.body.childNodes[0]);
 	}
+}
+
+function component(width, height, color, x, y) {
+	this.width = width;
+	this.height = height;
+	this.x = x;
+	this.y = y;    
+	ctx = myGameArea.context;
+	ctx.fillStyle = color;
+	ctx.fillRect(this.x, this.y, this.width, this.height);
 }
